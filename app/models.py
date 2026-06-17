efd4fc6e660b4b9af67bbeb7d43ca3ca299defe5
hoppa1231 <maksimkim005@gmail.com>
2026-06-17 04:39:52 +0300

    Refactor NotificationKind to use Enum instead of StrEnum

--- app/models.py
+++ app/models.py
@@ -1,9 +1,9 @@
-from enum import StrEnum
+from enum import Enum
 
 from pydantic import BaseModel, Field
 
 
-class NotificationKind(StrEnum):
+class NotificationKind(str, Enum):
     critical = "critical"
     info = "info"
     debug = "debug"
